6b98ee88111f2f8b942bf09ba90c496914626c37
arshan09 <cu.17bcs1934@gmail.com>
2023-09-21 12:16:29 +0530

    Added CORS

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const connectDB = require("./src/configs/DbConnect");
 require("dotenv").config();
-const errorHandler = require("./src/middlewares/errorHandler"); // Load environment variables from .env file
+const errorHandler = require("./src/middlewares/errorHandler");
 const profileRoutes = require("./src/routes/profileRoutes");
+const cors = require("cors"); // Import the cors middleware
 
 const app = express();
+
+// Use the cors middleware to enable CORS for all routes
+app.use(cors());
 
 app.use(express.json());
 
@@ -13,6 +17,7 @@ app.use(errorHandler);
 const port = process.env.PORT || 5000;
 
 app.use("/api", profileRoutes);
+
 /**
  * Start the application.
  * @function
